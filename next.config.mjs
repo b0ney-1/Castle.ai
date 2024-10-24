10933fcb1b62aebb269c450c22ef9a3b3f1c1505
boney <boney.fernandez@metaschool.so>
2024-10-24 17:54:27 +0530

    ignore CSR bailout

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@mui/icons-material"],
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 };
 
 export default nextConfig;
